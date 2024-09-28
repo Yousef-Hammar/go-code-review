4cf5a4f696369a12f65a0f80f04458bb7e713e28
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-28 16:50:28 +0200

    test: added repository FindByCode tests

---
+++ review/internal/repository/memory/repository_test.go
@@ -0,0 +1,68 @@
+package memory_test
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"coupon_service/internal/domain"
+	"coupon_service/internal/repository/memory"
+)
+
+func TestFindByCode(t *testing.T) {
+	type testCase struct {
+		name        string
+		code        string
+		expectedErr error
+		want        *domain.Coupon
+	}
+
+	testCases := []testCase{
+		{
+			name:        "Coupon found",
+			code:        "test",
+			expectedErr: nil,
+			want: &domain.Coupon{
+				ID:             "test",
+				Code:           "test",
+				Discount:       0,
+				MinBasketValue: 0,
+			},
+		},
+		{
+			name:        "Coupon not found",
+			code:        "not found",
+			expectedErr: memory.ErrNotFound,
+			want:        nil,
+		},
+		{
+			name:        "Empty coupon code",
+			code:        "",
+			expectedErr: memory.ErrNotFound,
+			want:        nil,
+		},
+	}
+
+	repo := memory.New()
+	_ = repo.Save(domain.Coupon{
+		ID:             "test",
+		Code:           "test",
+		Discount:       0,
+		MinBasketValue: 0,
+	})
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			coupon, err := repo.FindByCode(tc.code)
+			if tc.expectedErr != nil {
+				if !errors.Is(err, tc.expectedErr) {
+					t.Errorf("expected err to be %v, got %v", tc.expectedErr, err)
+					return
+				}
+			}
+			if !reflect.DeepEqual(tc.want, coupon) {
+				t.Errorf("expected coupon to be %v, got %v", tc.want, coupon)
+			}
+		})
+	}
+}
