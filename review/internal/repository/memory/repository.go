5d83dd6388d9d455170ed0635a9f4f1bb4543d49
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-28 16:57:21 +0200

    feat: added mutex in memory repository

--- review/internal/repository/memory/repository.go
+++ review/internal/repository/memory/repository.go
@@ -2,6 +2,7 @@ package memory
 
 import (
 	"errors"
+	"sync"
 
 	"coupon_service/internal/domain"
 )
@@ -10,23 +11,30 @@ var ErrNotFound = errors.New("coupon not found")
 
 type Repository struct {
 	entries map[string]domain.Coupon
+	mu      *sync.Mutex
 }
 
 func New() *Repository {
 	return &Repository{
 		entries: make(map[string]domain.Coupon),
+		mu:      &sync.Mutex{},
 	}
 }
 
 func (r *Repository) FindByCode(code string) (*domain.Coupon, error) {
-	coupon, ok := r.entries[code]
-	if !ok {
-		return nil, ErrNotFound
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if coupon, ok := r.entries[code]; ok {
+		return &coupon, nil
 	}
-	return &coupon, nil
+	return nil, ErrNotFound
 }
 
 func (r *Repository) Save(coupon domain.Coupon) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
 	r.entries[coupon.Code] = coupon
 	return nil
 }
