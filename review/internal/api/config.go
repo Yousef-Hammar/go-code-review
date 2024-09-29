41db4a732f00ce7d5c691a86dafd200d2bd94b29
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-29 22:01:51 +0200

    refactor: removed unused function

--- review/internal/api/config.go
+++ review/internal/api/config.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"os"
-	"strconv"
 )
 
 type Config struct {
@@ -19,15 +18,6 @@ func New() *Config {
 func getString(key string, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
-	}
-	return fallback
-}
-
-func getInt(key string, fallback int) int {
-	if value, ok := os.LookupEnv(key); ok {
-		if parsedValue, err := strconv.Atoi(value); err == nil {
-			return parsedValue
-		}
 	}
 	return fallback
 }
