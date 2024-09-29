ae59325dace25bbd52965d280635882cce6554db
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-29 15:46:38 +0200

    refactor: added coupon checks on coupon creation

--- review/internal/service/service.go
+++ review/internal/service/service.go
@@ -1,11 +1,19 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 
 	. "github.com/Yousef-Hammar/go-code-review/coupon_service/internal/domain"
+	"github.com/Yousef-Hammar/go-code-review/coupon_service/internal/repository/memory"
+)
+
+var (
+	ErrInvalidCode           = errors.New("invalid code")
+	ErrInvalidDiscount       = errors.New("invalid discount")
+	ErrInvalidMinBasketValue = errors.New("invalid min basket")
 )
 
 type Repository interface {
@@ -23,30 +31,28 @@ func New(repo Repository) Service {
 	}
 }
 
-func (s Service) ApplyCoupon(basket Basket, code string) (b *Basket, e error) {
-	b = &basket
-	coupon, err := s.repo.FindByCode(code)
-	if err != nil {
-		return nil, err
+func (s Service) CreateCoupon(discount int, code string, minBasketValue int) error {
+	if code == "" {
+		return ErrInvalidCode
 	}
 
-	if b.Value > 0 {
-		b.AppliedDiscount = coupon.Discount
-		b.ApplicationSuccessful = true
-	}
-	if b.Value == 0 {
-		return
+	if discount < 0 || discount > 100 {
+		return ErrInvalidDiscount
 	}
 
-	return nil, fmt.Errorf("Tried to apply discount to negative value")
-}
+	if minBasketValue < 0 {
+		return ErrInvalidMinBasketValue
+	}
 
-func (s Service) CreateCoupon(discount int, code string, minBasketValue int) any {
+	if _, err := s.repo.FindByCode(code); err == nil || !errors.Is(err, memory.ErrNotFound) {
+		return ErrInvalidCode
+	}
+
 	coupon := Coupon{
-		Discount:       discount,
-		Code:           code,
-		MinBasketValue: minBasketValue,
 		ID:             uuid.NewString(),
+		Code:           code,
+		Discount:       discount,
+		MinBasketValue: minBasketValue,
 	}
 
 	if err := s.repo.Save(coupon); err != nil {
@@ -72,4 +78,22 @@ func (s Service) GetCoupons(codes []string) ([]Coupon, error) {
 	}
 
 	return coupons, e
+}
+
+func (s Service) ApplyCoupon(basket Basket, code string) (b *Basket, e error) {
+	b = &basket
+	coupon, err := s.repo.FindByCode(code)
+	if err != nil {
+		return nil, err
+	}
+
+	if b.Value > 0 {
+		b.AppliedDiscount = coupon.Discount
+		b.ApplicationSuccessful = true
+	}
+	if b.Value == 0 {
+		return
+	}
+
+	return nil, fmt.Errorf("Tried to apply discount to negative value")
 }
