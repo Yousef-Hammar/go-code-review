4b93ba8d2cd9e8e7bef5b2ae06a0b2ee7ccee533
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-29 22:12:23 +0200

    refactor: removed unused init function

--- review/cmd/coupon_service/main.go
+++ review/cmd/coupon_service/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"runtime"
 
 	"go.uber.org/zap"
 
@@ -11,18 +9,6 @@ import (
 	"github.com/Yousef-Hammar/go-code-review/coupon_service/internal/repository/memory"
 	"github.com/Yousef-Hammar/go-code-review/coupon_service/internal/service"
 )
-
-const (
-	localEnv = "local"
-	numCPU   = 32
-)
-
-func init() {
-	if os.Getenv("env") != localEnv && runtime.NumCPU() != numCPU {
-		log.Print("this api is meant to be run on 32 core machines")
-		os.Exit(1)
-	}
-}
 
 func main() {
 	config := api.New()
