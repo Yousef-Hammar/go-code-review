c3dc6c4b7893f22d86f4a9b1097e17f336daa6d2
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-29 22:46:56 +0200

    feat: added requestLoggerMiddleware

--- review/internal/api/api.go
+++ review/internal/api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -24,15 +26,48 @@ func NewApplication(config Config, logger *zap.SugaredLogger, service Service) *
 	}
 }
 
+func (app *Application) requestLoggerMiddleware(c *gin.Context) {
+	var requestBody []byte
+	now := time.Now().UTC()
+
+	if c.Request.Body != nil {
+		bodyBytes, err := io.ReadAll(c.Request.Body)
+		if err != nil {
+			app.logger.Error("Failed to read request body", zap.Error(err))
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		requestBody = bodyBytes
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	}
+
+	c.Next()
+
+	latency := time.Since(now)
+
+	app.logger.Info("Request",
+		zap.String("method", c.Request.Method),
+		zap.String("path", c.Request.URL.Path),
+		zap.String("query", c.Request.URL.RawQuery),
+		zap.String("body", string(requestBody)),
+		zap.String("clientIP", c.ClientIP()),
+		zap.Int("status", c.Writer.Status()),
+		zap.Duration("latency", latency),
+	)
+
+	c.Request.Body = io.NopCloser(bytes.NewReader(requestBody))
+}
+
 func (app *Application) Mount() http.Handler {
 	router := gin.New()
+	router.Use(app.requestLoggerMiddleware)
 
 	v1 := router.Group("/v1")
 
 	coupons := v1.Group("/coupons")
 	{
-		coupons.POST("/", app.Create)
-		coupons.GET("/", app.Get)
+		coupons.POST("", app.Create)
+		coupons.GET("", app.Get)
 		coupons.POST("/basket", app.Apply)
 	}
 
@@ -48,7 +83,9 @@ func (app *Application) Run(mux http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
-	app.logger.Info("start http server on", "addr", srv.Addr)
+	app.logger.Info("Listening",
+		zap.String("on", srv.Addr),
+	)
 
 	if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
 		app.logger.Errorw("start http server failed", "error", err)
