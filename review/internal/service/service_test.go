eb14d84d4ee7fb5c935585a27c1a89817851a70b
Yousef Hammar <yousef.hammar@gmail.com>
2024-09-29 11:35:32 +0200

    refactor: removed service constructor test

--- review/internal/service/service_test.go
+++ review/internal/service/service_test.go
@@ -8,26 +8,6 @@ import (
 	"coupon_service/internal/repository/memory"
 )
 
-func TestNew(t *testing.T) {
-	type args struct {
-		repo Repository
-	}
-	tests := []struct {
-		name string
-		args args
-		want Service
-	}{
-		{"initialize service", args{repo: nil}, Service{repo: nil}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.repo); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestService_ApplyCoupon(t *testing.T) {
 	type fields struct {
 		repo Repository
